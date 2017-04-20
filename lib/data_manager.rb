607dc644cde96a36ce786295a52ef39e8b238207
Jesse Rivera <jrivera@sccadv.com>
2017-04-20 14:52:42 -0500

    Clean up file

--- lib/data_manager.rb
+++ lib/data_manager.rb
@@ -1,45 +1,48 @@
 module DataManager
-	#
-	# On extension of the module
-	# check for existance of data directory and data file
-	# and create if they do not exist
-	#
-	def self.extended(obj)
-		@@data_dir = "db"
-		@@data_file = "#{@@data_dir}/#{obj.class.name.downcase}.txt"
 
-		make_data_dir
-		make_data_file
-	end
+  # Directory where data will get stored
+  DATA_DIR = "db"
 
-	def write_data(contents)
-		File.open(@@data_file, 'a') { |f| f << "#{contents}\n" }
-	end
+  #
+  # On extension of the module
+  # check for existance of data directory and data file
+  # and create if they do not exist
+  #
+  def self.extended(obj)
+    @@data_file = "#{DATA_DIR}/#{obj.class.name.downcase}.txt"
 
-	def read_data
-		File.read(@@data_file)
-	end
+    make_data_dir
+    make_data_file
+  end
 
-	def save
-		values = instance_variables.map { |v| instance_variable_get(v) }
-		write_data(values.join(","))
-	end
+  def write_data(contents)
+    File.open(@@data_file, 'a') { |f| f << "#{contents}\n" }
+  end
 
-	private
+  def read_data
+    File.read(@@data_file)
+  end
 
-	#
-	# Check if the data directory exists
-	# if not create one
-	#
-	def self.make_data_dir
-		Dir.mkdir(@@data_dir) unless File.directory?(@@data_dir)
-	end
+  def save
+    values = instance_variables.map { |v| instance_variable_get(v) }
+    write_data(values.join(","))
+  end
 
-	#
-	# Check if the data file exists
-	# if not create one named after the class that extended the module
-	# 
-	def self.make_data_file
-		File.new(@@data_file, 'w') unless File.file?(@@data_file)
-	end
+  private
+
+  #
+  # Check if the data directory exists
+  # if not create one
+  #
+  def self.make_data_dir
+    Dir.mkdir(DATA_DIR) unless File.directory?(DATA_DIR)
+  end
+
+  #
+  # Check if the data file exists
+  # if not create one named after the class that extended the module
+  # 
+  def self.make_data_file
+    File.new(@@data_file, 'w') unless File.file?(@@data_file)
+  end
 end
